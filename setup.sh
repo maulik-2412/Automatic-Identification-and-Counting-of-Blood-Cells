20bc39274672e4cdef2be6566368ac5d38c881d4
Maulik Tyagi <mauliktyagi08@gmail.com>
2024-11-13 00:43:58 +0530

    chnages

--- setup.sh
+++ setup.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-echo "Installing dependencies"
-pip install --upgrade pip
-pip install -r requirements.txt
-
-# Ensure Cython is installed
 pip install cython
-
-echo "Building Cython files"
 python setup.py build_ext --inplace
